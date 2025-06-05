17c304cc52a31dcc752c0bd6951efa70aaea2472
kauetrnt <rm559317@fiap.com.br>
2025-06-05 13:07:38 -0300

    Remove ProtectedRoute component from Contato, Integrantes, and Riscos pages to simplify layout; enhance user experience by streamlining component structure.

--- src/app/integrantes/page.tsx
+++ src/app/integrantes/page.tsx
@@ -1,13 +1,10 @@
 import Membros from '../components/Membros/Membros';
-import ProtectedRoute from '../components/ProtectedRoute/ProtectedRoute';
 
 export default function Integrantes() {
     return (
-        <ProtectedRoute>
-            <div className="min-h-screen bg-[#132536] pt-10 pl-10 pr-10 lg:pl-20 lg:pr-20 md:pl-15 md:pr-15">
-                <h1 className="font-semibold text-2xl text-[#F5FAFF]">Integrantes - TDSPS</h1>
-                <Membros />
-            </div>
-        </ProtectedRoute>
+        <div className="min-h-screen bg-[#132536] pt-10 pl-10 pr-10 lg:pl-20 lg:pr-20 md:pl-15 md:pr-15">
+            <h1 className="font-semibold text-2xl text-[#F5FAFF]">Integrantes - TDSPS</h1>
+            <Membros />
+        </div>
     )
 }
--- src/app/riscos/page.tsx
+++ src/app/riscos/page.tsx
@@ -1,18 +1,15 @@
 'use client';
 
 import Dashboard from '../components/Dashboard/Dashboard';
-import ProtectedRoute from '../components/ProtectedRoute/ProtectedRoute';
 export default function Riscos() {
     return (
-        <ProtectedRoute>
-            <div className="min-h-screen bg-[#132536] pt-10 pl-10 pr-10 lg:pl-20 lg:pr-20 md:pl-15 md:pr-15">
-                <div className="flex flex-col items-left justify-center">
-                    <h1 className="text-2xl font-semibold text-left text-[#F5FAFF] pb-8">Dashboard de riscos</h1>
-                    <div className="w-full flex items-center justify-center">
-                        <Dashboard />
-                    </div>
+        <div className="min-h-screen bg-[#132536] pt-10 pl-10 pr-10 lg:pl-20 lg:pr-20 md:pl-15 md:pr-15">
+            <div className="flex flex-col items-left justify-center">
+                <h1 className="text-2xl font-semibold text-left text-[#F5FAFF] pb-8">Dashboard de riscos</h1>
+                <div className="w-full flex items-center justify-center">
+                    <Dashboard />
                 </div>
             </div>
-        </ProtectedRoute>
+        </div>
     )
 }
