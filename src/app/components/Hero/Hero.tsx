cbaa328808c6823b1671ddfe01fe16b9e0c704c2
kauetrnt <rm559317@fiap.com.br>
2025-06-05 14:16:31 -0300

    Refactor Hero component layout and styling; adjust padding and text sizes for improved responsiveness and visual consistency across different screen sizes.

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -16,22 +16,22 @@ export default function Hero() {
                     backgroundPosition: "60% center",
                 }}
             >
-                <div className="px-8 md:px-4 md:pl-60 md:pr-16 w-full md:w-2/5 text-white text-3xl md:text-5xl font-bold">
+                <div className="px-8 md:pl-15 xl:pl-40 w-full md:w-2/4 xl:w-2/5 text-white text-3xl xl:text-5xl font-bold">
                     <h1>
                         Enchentes não podem mais te pegar de surpresa
                     </h1>
-                    <span className="text-white text-base md:text-3xl font-medium">
+                    <span className="text-white text-base xl:text-3xl font-medium">
                         SafeFlood antecipa riscos, protege vidas e conecta sua cidade em tempo real.
                     </span>
                     <div className="flex flex-col md:flex-row gap-4 pt-4">
-                        <Button className="w-full bg-white"
+                        <Button className="w-full bg-white md:text-sm xl:text-base"
                             onClick={() => {
                                 router.push('/riscos');
                             }}>
                             Conheça o SafeFlood
                         </Button>
                         <Button
-                            className="w-full"
+                            className="w-full md:text-sm xl:text-base"
                             onClick={() => {
                                 router.push('/riscos');
                             }}>
